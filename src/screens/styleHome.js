314fc0a8c71d8819ac2bfc8e7c95bc4fb85de742
Eduardo Batael <eduardobatael9@gmail.com>
2026-02-23 10:25:32 -0300

    criação dos arquivos de estilo e a conexão com o arquivo
    23/02/2026 criação e ligação dos arquivos de estilo, mentoria professor James.

--- src/screens/styleHome.js
+++ src/screens/styleHome.js
@@ -1,3 +1,19 @@
-import { StyleSheet }  from 'react-native';
+import { StyleSheet } from "react-native";
 
-const 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor: '#f5f5f5',
+        paddingTop: 50,
+    },
+    headerTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginLeft: 20,
+        marginBottom: 20,
+        color: '#333',
+    },
+    list: {
+        paddingHorizontal: 20,
+    }
+});
